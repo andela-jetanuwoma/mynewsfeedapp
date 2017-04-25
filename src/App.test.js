85d8b03e4b25c8bee86506b840410d7c9b85c5e5
wapjude <wapjude@gmail.com>
2017-04-25 13:25:03 +0100

    write test for user authentication

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './controllers/App';
+import User from './models/user';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+const uservalue = {name:"jude peter",email:"wapjude@gmail.com",imageUrl:"some_picture.jpg"};
+
+it('should log user in when values is supplied and store it',()=>{ 
+	User.Login(uservalue);
+	expect(User.isLoggedIn()).toBe(true);
+	
+});
+
+it('should return jude peter as the name of the logged in user',() => {
+	expect(User.name).toEqual("jude peter");
+});
+
+it('should return wapjude@gmail.com as the logged in user', ()=>{
+	expect(User.email).toEqual("wapjude@gmail.com");
+});
+
+it('should return some_picture.jpg as logged in', () => {
+	expect(User.imageUrl).toEqual("some_picture.jpg");
+});
+
+it('should log user out and remove all user details', () => {
+	expect(User.logOut()).toBe(true);
+});
+it('should be undefined for user name',()=>{
+	expect(User.name).toBe(undefined);
+});
+
+it('should be undefined for user email',()=>{
+	expect(User.email).toBe(undefined);
+});
+
+it('should be undefined for user imageUrl',()=>{
+	expect(User.imageUrl).toBe(undefined);
+});
+
+
+
---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+import Cookies from 'js-cookie'
+class User {
+  constructor(){
+    
+  }
+  isLoggedIn(){
+     
+  
+  }
+
+  assignUserValues(){
+   
+  }
+  
+  Login(context){
+   
+  }
+
+  addFavorites(newItem){
+   
+  }
+
+  removeFavourite(item,index){
+    
+  }
+  logOut(){
+    
+  }
+}
+export default new User();
