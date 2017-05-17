979132145472cb7cd00d845a1a10a636af487713
wapjude <wapjude@gmail.com>
2017-05-17 09:44:55 +0100

    Feedback implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './assets/style/index.scss';
-import '../semantic/dist/semantic.min.css';
+import './assets/style/index.css';
+import './assets/semantic/dist/semantic.min.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
